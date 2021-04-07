a3e4560f5f00af582480cfc0739feb26d534c160
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-04-06 23:02:50 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Door from './components/Door'
+
+function App() {
+  return (
+    <div className="container">
+<Door/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Door.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import door_closed5 from '../assets/door_closed5.jpg'
+
+
+
+const Door = () => {
+  const [isLeftDoorSelected, setLeftDoorSelected] = useState(door_closed5)
+  const [isRightDoorSelected, setRightDoorSelected] = useState('')
+
+
+  // const doorsRight = isRightDoorSelected ? door_open2dr : door_closed5
+
+  return (
+    <div className="door_container">
+      {/* <button
+        onClick={() => setLeftDoorSelected()}
+        className="key_button"
+      ></button>
+      <button
+        onClick={() => setRightDoorSelected(door_closed5)}
+        className="key_button left"
+      ></button> */}
+
+      <img src={door_closed5} className="door_left" alt={'doors'} />
+      <img src={door_closed5} className="door_right" alt={'doors'} />
+
+
+    </div>
+  )
+}
+export default Door
